c7ec4c70b04df599c6e49975a9f73f6eb03bc99d
zoltroak <zoltroak@gmail.com>
2025-02-18 03:33:41 +0500

    feat(core): add EchoClient and EchoError classes, update types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,36 @@
+export type ValueOf<T> = T[keyof T]
+
+// ----Enum
+
+export const EchoMethod = {
+	GET: 'GET',
+	POST: 'POST',
+	PUT: 'PUT',
+	PATCH: 'PATCH',
+	DELETE: 'DELETE'
+} as const
+export type EchoMethod = ValueOf<typeof EchoMethod>
+
+export const EchoResponseType = {
+	JSON: 'json',
+	TEXT: 'text',
+	ARRAY_BUFFER: 'arrayBuffer',
+	BLOB: 'blob',
+	BYTES: 'bytes',
+	FORM_DATA: 'formData',
+	STREAM: 'stream',
+	ORIGINAL: 'original'
+} as const
+export type EchoResponseType = ValueOf<typeof EchoResponseType>
+
+export const EchoInterceptors = {
+	REQUEST: 'request',
+	RESPONSE: 'response'
+} as const
+export type EchoInterceptors = ValueOf<typeof EchoInterceptors>
+
+// ----Const
+
 export type EchoSearchParams = {
 	[key: string]:
 		| string
@@ -6,4 +39,33 @@ export type EchoSearchParams = {
 		| null
 		| undefined
 		| Array<string | number | boolean | null | undefined>
+}
+
+// ----Config
+
+export type EchoConfig = Omit<RequestInit, 'method' | 'headers' | 'body'> & {
+	method: EchoMethod
+	url: string
+	baseURL?: string
+	params?: EchoSearchParams
+	headers?: Record<string, string>
+	responseType?: EchoResponseType
+	body?: any
+}
+export type EchoCreateConfig = Omit<EchoConfig, 'url' | 'method'>
+
+// ----Request
+
+export type EchoRequest = Omit<EchoConfig, 'baseURL' | 'params'>
+export type EchoRequestOptions = EchoCreateConfig
+
+// ----Response
+
+export type EchoResponse<T = any> = {
+	data: T
+	status: number
+	statusText: string
+	headers: Record<string, string>
+	config: EchoConfig
+	request: EchoRequest
 }
